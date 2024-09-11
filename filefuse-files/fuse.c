8197e22da9610de1e2b2b8298833c6de05f41d93
juvereturn@gmail.com <Rusty05151>
2024-09-11 13:38:46 -0700

    Refactor the code and makes it work in C

--- filefuse-files/fuse.c
+++ filefuse-files/fuse.c
@@ -1,40 +1,58 @@
+/*
+Code By: Ju-ve Chankasemporn
+E-mail: juvereturn@gmail.com
+Brief: Fuse and unfuse files
+*/
+
+
+
 #include "fuse.h"
 #include <stdio.h>    /* fopen/fclose/fread/fwrite */
 #include <stdlib.h>   /* malloc/free */
 #include <string.h>   /* strlen */
 
 #define xDEBUG
+#define MAXIMUM_FILE_SIZE 256
+#define BUFFER_SIZE (1 << 16) /*Use For Reading the Filename*/
+
+/*
+Brief: 
+Loop through given filesnames
+Open the file, and write the file name
+Write the file size by using fseek and ftell to calculate the filesize
+*/
 
 int fuse( char const ** filenames, int num_files, char const * output)
 {
-    int BUFFER_SIZE = 1<<16; /* 65 Kb */
-    
-    FILE *out_file = fopen(output, "wb");  
+    FILE *out_file; 
+    FILE *read_file;
+    int index;
+    int file_size;
+    char * buffer;
 
+    out_file = fopen(output, "wb");  
     if (out_file == NULL) {
         perror("Unable to open output file");
         return -1;
     }
-    int i;
-    for (i = 0; i < num_files; i++) {
-        FILE *read_file = fopen(filenames[i], "rb");  
 
+    for (index = 0; index < num_files; index++) 
+    {
+        read_file = fopen(filenames[index], "rb");  
         if (read_file == NULL) {
             perror("Unable to open the read file");
             fclose(out_file);
             return -1;
         }
 
-        fwrite(filenames[i], sizeof (char), strlen(filenames[i]) + 1, out_file);
+        /* Add +1 to strlen to include the null terminator ('\0') in the file*/
+        fwrite(filenames[index], sizeof (char), strlen(filenames[index]) + 1, out_file);
 
-        fseek(read_file, 0L, SEEK_END );
-        int file_size = ftell( read_file );
-        fseek(read_file, 0L, SEEK_SET );
+        /*Find filesize*/
+        file_size = calculate_and_write_filesize( read_file, out_file);
 
-        fwrite(&file_size, sizeof(int), 1, out_file);
-
-        char * buffer = (char*)malloc(BUFFER_SIZE*sizeof(char));
-
+        /*Create a buffer for read and write data*/
+        buffer = (char*)malloc(BUFFER_SIZE*sizeof(char));
         if(buffer == NULL)
         {
             perror("Unable to read the buffer");
@@ -42,9 +60,10 @@ int fuse( char const ** filenames, int num_files, char const * output)
             fclose(read_file);
             return -1;
         }
+        read_and_write_data(buffer, file_size,  read_file,  out_file);
 
-        fread(buffer, sizeof(char), file_size, read_file);
-        fwrite(buffer, sizeof(char), file_size, out_file);
+        free(buffer);
+        fclose(read_file);
     }
 
     fclose(out_file);
@@ -52,12 +71,14 @@ int fuse( char const ** filenames, int num_files, char const * output)
 }
 
 int unfuse( char const * filename )
-{
-     printf("unfused");
-    int BUFFER_SIZE = 1<<16; 
-    
-    FILE *read_file = fopen(filename, "rb");  
+{   
+    FILE* read_file;
+    FILE* out_file;
+    char* out_fileName; 
+    int file_size;
+    char* buffer;
 
+    read_file = fopen(filename, "rb");  
     if (read_file == NULL) {
         perror("Unable to open read_file");
         return -1;
@@ -65,51 +86,76 @@ int unfuse( char const * filename )
 
     while(!feof(read_file))
     {
-        char ch;
-        char* out_fileName = (char*)malloc(BUFFER_SIZE * sizeof(char));
-        int i = 0;
-    
-        while ((ch = fgetc(read_file)) != EOF) {
-            
-            if (ch == '\0') {
-                out_fileName[i] = '\0'; 
-                break;
-            }
+        out_fileName = (char*)malloc(BUFFER_SIZE * sizeof(char));
+        get_filename(out_fileName, read_file);
 
-            out_fileName[i] = ch;
-            i++;
-        }
-
-        printf("filename is %s", out_fileName);
-
-        FILE *out_file = fopen(out_fileName, "wb");
-
+        out_file = fopen(out_fileName, "wb");
         if (out_file == NULL) {
             printf("Unable to open output file %s", out_fileName);
+            free(out_fileName);
             return -1;
         }
 
-        int file_size;
-        fread(&file_size, sizeof(int), 1, read_file);
-        char * buffer = (char*)malloc(file_size*sizeof(char));
+        /*Read the file size*/
 
+        fread(&file_size, sizeof(int), 1, read_file);
+
+        /*Create a buffer for read and write data*/
+
+        buffer = (char*)malloc(file_size*sizeof(char));
         if(buffer == NULL)
         {
             perror("Unable to read the buffer");
+            free(buffer);
             fclose(out_file);
             fclose(read_file);
             return -1;
         }
-
-        fread(buffer, sizeof(char), file_size, read_file);
-        fwrite(buffer, sizeof(char), file_size, out_file);
+        read_and_write_data(buffer, file_size,  read_file,  out_file);
 
         free(buffer);
         free(out_fileName);
         fclose(out_file);
-
     }
 
     fclose(read_file);
     return 0;
+}
+
+/*Applicable On Both Fuse And Unfuse Functions*/
+void read_and_write_data(char * buffer, int file_size, FILE* read_file, FILE* out_file)
+{
+    fread(buffer, sizeof(char), file_size, read_file);
+    fwrite(buffer, sizeof(char), file_size, out_file);
+}
+
+/*Fuse Functions*/
+int calculate_and_write_filesize(FILE* read_file, FILE* out_file)
+{
+    int file_size;
+    fseek(read_file, 0L, SEEK_END );
+    file_size = ftell( read_file );
+    fseek(read_file, 0L, SEEK_SET );
+    fwrite(&file_size, sizeof(int), 1, out_file);
+    return file_size;
+}
+
+/*Unfuse Functions*/
+void get_filename(char* filename, FILE* read_file)
+{
+    char ch;
+    int i;
+    i = 0;
+    
+   while ((ch = fgetc(read_file)) != EOF) 
+   {
+        if (ch == '\0') 
+        {
+            filename[i] = '\0'; 
+            break;
+        }
+
+        filename[i] = ch;
+        i++;
+    }
 }
--- filefuse-files/fuse.h
+++ filefuse-files/fuse.h
@@ -1,13 +1,24 @@
+#ifdef __cplusplus
+extern "C" { 
+#endif
+
 #ifndef FUSE_H
 #define FUSE_H
 
 #include <stddef.h> /* size_t definition */
+#include <stdio.h>  
 
 enum 
 {E_BAD_SOURCE=1, E_BAD_DESTINATION, E_NO_MEMORY };
 
 int unfuse( char const * filename );
 int fuse( char const ** filenames, int num_files, char const * output);
+/*Applicable On Both Fuse And Unfuse Function*/
+void read_and_write_data(char * buffer, int file_size,FILE* read_file, FILE* out_file);
+/*Fuse Function*/
+int calculate_and_write_filesize(FILE* read_file, FILE* out_file);
+/*Unfuse Function*/
+void get_filename(char* filename, FILE* read_file);
 
 #endif 
 
@@ -17,3 +28,6 @@ int fuse( char const ** filenames, int num_files, char const * output);
 |\0 terminated| int    |           |\0 terminated| int    |           |....
 +-------------+--------+-----------+-------------+--------+-----------+....
 */
+#ifdef __cplusplus
+}
+#endif
