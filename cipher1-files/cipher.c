0c14ba44e58e02ae499030b9f13d6fb6e7764f96
juvereturn@gmail.com <Rusty05151>
2024-09-24 17:09:29 -0700

    add comment

--- cipher1-files/cipher.c
+++ cipher1-files/cipher.c
@@ -1,3 +1,38 @@
+/*
+Auther: Ju-ve Chankasemporn
+E-mail: juvereturn@gmail.com
+Brief: Encoding each alphabet into bit numbers
+   0  a <-> 0     Group 0 (00) requires only 1 bit to encode 1 character
+   1  b <-> 1     b is encoded as 001 (00 specifies group, 1 is the index inside the group
+   ----------
+   2  c <-> 00     Group 1 (01) requires only 2 bits to encode 1 character
+   3  d <-> 01
+   4  e <-> 10     e is encoded as 0110 (01 specifies group, 10 is the index inside the group
+   5  f <-> 11
+   ----------
+   6  g <-> 000     Group 2 (10) requires only 3 bits to encode 1 character
+   7  h <-> 001
+   8  i <-> 010
+   9  j <-> 011     j is encoded as 10011 (10 specifies group, 011 is the index inside the group
+   10 k <-> 100
+   11 l <-> 101
+   12 m <-> 110
+   13 n <-> 111
+   ----------
+   14 o <-> 0000     Group 3 (11) requires only 4 bits to encode 1 character
+   15 p <-> 0001
+   16 q <-> 0010
+   17 r <-> 0011
+   18 s <-> 0100
+   19 t <-> 0101     t is encoded as 110101 (11 specifies group, 0101 is the index inside the group
+   20 u <-> 0110
+   21 v <-> 0111
+   22 w <-> 1000
+   23 x <-> 1001
+   24 y <-> 1010
+   25 z <-> 1011
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -62,8 +97,6 @@ void decode( char const* ciphertext, int num_chars, char* plaintext )
 /* assume all lower case input a-z (no spaces) */
 void encode( char const * plaintext, char* encryptedtext, int *num_bits_used ) 
 {
- /*not used,
-  but the actual number of bytes needed for encodedtext is num_bits_used/8 + (num_bits_used%8)?1:0*/
     int group_number;
     int encoding_position;
     *num_bits_used = 0;
@@ -71,34 +104,63 @@ void encode( char const * plaintext, char* encryptedtext, int *num_bits_used )
     while(*plaintext != '\0')
     {
         group_number = get_group_number(*plaintext);
+        if(group_number == -1)
+        {
+            perror("invalid character");
+            return -1;
+        }
+
         encoding_position = get_encoding_position(*plaintext, group_number);
+        if(encoding_position == -1)
+        {
+            perror("invalid character");
+            return -1;
+        }
+
         append_to_buffer(encryptedtext, num_bits_used, group_number, 2); 
         append_to_buffer(encryptedtext, num_bits_used, encoding_position, group_number + 1); 
         plaintext++;
     }
-
-    encryptedtext[strlen(encryptedtext)] = '\0';
 }
 
-/*Function to append binary representation of an integer to the buffer*/
+/*
+Brief: 
+turn int value into bitnumber, then write it to buffer
+Further Explanation: 
+        buffer[*num_bits_used / 8] is the location where bit will be written. 
+            Note that: buffer[index] will contain up to 8 bits. eg: buffer[0] = 01001111
+        (1 << (*num_bits_used % 8)) is used to write/delete bit
+            Note that: modular % will give you the position
+*/
 void append_to_buffer(char *buffer, int *num_bits_used, int value, int bits) 
 {
     int i;
     for (i = 0; i < bits; i++) 
     {
+        /*
+        value & 1 means that the right most value is 1
+        */
         if (value & 1) 
         {
+            /* |= => add 1 */
             buffer[*num_bits_used / 8] |= (1 << (*num_bits_used % 8));
         } 
         else 
         {
+            /* &= ~ => delete 1 */
             buffer[*num_bits_used / 8] &= ~(1 << (*num_bits_used % 8));
         }
+        
         value >>= 1;
         (*num_bits_used)++;
     }
 }
 
+/*
+Brief:
+all return value will be converted to bit number (later on: append_to_buffer)
+eg. ('x' - 'o' => |-10|) ->  10 => 1010
+*/
 int get_encoding_position(char character, int group_pos)
 {
     switch (group_pos)
@@ -118,6 +180,11 @@ int get_encoding_position(char character, int group_pos)
     return -1;
 }
 
+/*
+Brief:
+all return value will be converted to bit number  (later on: append_to_buffer)
+eg. 3 => 11
+*/
 int get_group_number(char character)
 {
     if(character >= 'a' && character <= 'b')
