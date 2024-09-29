cfca412aa0f6b50b0a7f296fe424541e12caffd4
juvereturn@gmail.com <Rusty05151>
2024-09-28 19:32:50 -0700

    implement spvector

---
+++ sparsevector-files/spvector.c
@@ -0,0 +1,227 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "spvector.h" 
+/* definition of Node is hidden from the client (driver) so that client CANNOT
+ * use INTERNALS of the Node.  this is poor-man encapsulations - hiding
+ * implementation details.  The main reason for encapsulations -- if I ever
+ * decide to change Node struct no client code will break. Example: I decide to
+ * change "left" to "Left", all I do is change map.h and map.c, no client code
+ * is effected, since clients were FORCED to use getleft(...) function, rather
+ * than field name "left".  Also see typedef in map.h
+ */
+struct ElementNode {
+  int    data;
+  int    pos;
+  struct ElementNode* next;
+};
+
+typedef struct ElementNode ElementNode;
+typedef struct RowNode RowNode;
+
+/*print functions*/
+void printf_elements(ConstElementNode_handle p_e, char const *fmt, int dim) 
+{
+	int i,last_pos=-1;
+	while (p_e) 
+    {
+		for (i=last_pos+1;i<p_e->pos;++i) 
+    { 
+      printf(fmt,0); 
+    }
+		printf(fmt,p_e->data);
+		last_pos=p_e->pos;
+		p_e = p_e->next;
+	}
+	for (i=last_pos+1;i<dim;++i) 
+    {
+         printf(fmt,0); 
+    }
+}
+
+void print_elements(ConstElementNode_handle p_e)
+ {
+	while (p_e) 
+    {
+		printf("%i at pos %i, ",p_e->data,p_e->pos);
+		p_e = p_e->next;
+	}
+}
+
+int insert_element(ElementNode_handle *p_e, int pos, int value)
+{
+    if(value == 0)
+    {
+        return 1;
+    }
+    
+    ElementNode *element_node = (struct ElementNode *) malloc(sizeof(struct ElementNode));
+    if(element_node == NULL)
+    {
+        return 1;
+    }
+
+    element_node->data = value;
+    element_node->pos = pos;
+
+    if((*p_e) == NULL)
+    {
+        (*p_e) = element_node;
+    }
+    else
+    {
+        struct ElementNode *temp = (*p_e), *prev;
+
+        // Find the correct position in the list for the new node. 
+        // temp will point to an node after the point of insertion
+        // that's why we need prev
+        while (temp && (temp->pos < element_node->pos)) 
+        {
+            prev = temp;       // save previous (singly linked)     
+            temp = temp->next;
+        }
+
+        // If this number comes before the first one
+        // insert at head, modify head
+        if (temp == (*p_e)) {
+            (*p_e) = element_node;
+        }
+        else
+        {
+            prev->next = element_node; // Insert between current and prev node
+        }
+
+        element_node->next = temp; 
+    }  
+
+    return 0;
+}
+
+void delete_element( ElementNode_handle *p_e, int pos)
+{
+  struct ElementNode *curr = (*p_e);
+  struct ElementNode *prev = NULL;
+   
+  while (curr && curr->pos != pos)
+  {
+    prev = curr;
+    curr = curr->next;
+  }
+ 
+  if (curr == NULL)
+  {
+    return;
+  }
+    
+  if (prev == NULL)
+  {
+    (*p_e) = (*p_e)->next;
+  }
+  else
+  {
+    prev->next = curr->next;
+  }
+
+  free(curr);
+}
+
+/*
+ * get the value at the given position,
+ * p_e is the head pointer 
+ */
+int get( ConstElementNode_handle p_e, int i )
+{
+  while (p_e && p_e->pos <= i) 
+  {
+    if(p_e->pos == i)
+    {
+      return p_e->data;
+    }
+    p_e = p_e->next;
+	}
+  return 0;
+}
+
+/* 
+ * scalar product of 2 lists.
+ * */
+int scalar_product(ConstElementNode_handle p_e, ConstElementNode_handle p_j)
+{
+    int value;
+    value = 0;
+    if(p_e == NULL || p_j == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+      while(p_e && p_j)
+      {
+        if(p_e->pos < p_j->pos)
+        {
+          p_e = p_e->next;
+        }
+        else if(p_e->pos > p_j->pos)
+        {
+          p_j = p_j->next;
+        }
+        else if(p_e->pos == p_j->pos)
+        {
+          value += p_e->data * p_j->data;
+          p_e = p_e->next;
+          p_j = p_j->next;
+        }
+      }
+
+      return value;
+    }
+}
+
+/* 
+ * adds 2 lists as vectors, returns a new list */
+ElementNode_handle add( ConstElementNode_handle p_e,ConstElementNode_handle p_j)
+{
+  ElementNode_handle result_node = NULL;
+
+    while(p_e && p_j)
+      {
+        if(p_e->pos < p_j->pos)
+        {
+          insert_element(&result_node, p_e->pos, p_e->data);
+          p_e = p_e->next;
+        }
+        else if(p_e->pos > p_j->pos)
+        {
+          insert_element(&result_node, p_j->pos, p_j->data);
+          p_j = p_j->next;
+        }
+        else if(p_e->pos == p_j->pos)
+        {
+          insert_element(&result_node, p_e->pos, p_e->data + p_j->data);
+          p_e = p_e->next;
+          p_j = p_j->next;
+        }
+      }
+
+    // Add remaining elements from p_e
+    while (p_e) 
+    {
+        insert_element(&result_node, p_e->pos, p_e->data);
+        p_e = p_e->next;
+    }
+
+    // Add remaining elements from p_j
+    while (p_j) 
+    {
+        insert_element(&result_node, p_j->pos, p_j->data);
+        p_j = p_j->next;
+    }
+
+  return result_node;
+}
+
+void free_elements(ElementNode_handle p_e)
+{
+  struct ElementNode *temp = p_e->next; 
+  free(p_e);                   
+  p_e = temp;  
+}
